a306a3ea500240312aed21c074be98c5193bb435
SHOHAG KUMAR <118052454+MayerShohag@users.noreply.github.com>
2023-09-22 22:45:42 +0600

    Add files via upload
    π মিটার ব্যাসার্ধ বিশিষ্ট একটি বৃত্তের ক্ষেত্রফল নির্ণয়ের পাইথন প্রোগ্রাম লিখ।

---
+++ Area of a circle.py
@@ -0,0 +1,5 @@
+import math
+radius = float(input("Enter the value of radius: ")) #ব্যাসার্ধ ইনপুট দিতে হবে
+area = math.pi*radius*radius #ক্ষেত্রফলে সূত্র বসানো হয়েছে
+print("Area is %.2f"%area) #প্রদর্শন করা হয়েছে
+
